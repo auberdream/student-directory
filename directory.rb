cb942f6b86cab24a19988a4b4a1d1b1a95a85764
Ellie <elliewem@gmail.com>
2017-12-12 16:08:36 +0000

    adds method to filter by cohort

--- directory.rb
+++ directory.rb
@@ -5,9 +5,7 @@ def print_header
 end
 
 def print_names(names)
-  names.each do |students|
-    puts "#{students[:name]} (#{students[:cohort]} cohort)".center(80)
-  end
+  names.select { |students| puts "#{students[:name]}, #{students[:cohort]} cohort".center(80) if students[:cohort] == :November }
 end
 
 def print_footer(names)
@@ -15,17 +13,29 @@ def print_footer(names)
 end
 
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the details of the students"
   puts "To finish, just hit return twice"
   students =[]
-  name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
+  loop do
+    puts "Name?"
+    name = gets.chomp.downcase.capitalize
+    if name.empty? == true
+      break loop
+    end
+    puts "#{name}'s cohort?"
+    cohort = gets.chomp.downcase
+    if ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"].include?(cohort) == false
+      puts "Are you sure that's a month? Try again! If you're not sure of the cohort at this time, simply press enter."
+      cohort = gets.chomp.downcase
+      if cohort == ""
+        cohort = "Unknown"
+      end
+    end
+    students << {name: name, cohort: cohort.capitalize.to_sym}
   end
   students
 end
+
 
 students = input_students
 print_header
